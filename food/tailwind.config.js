f0afe3eabd3e5b8e324f149dbcadcbe50871c233
SaiiMaii <tanaponrxmai@gmail.com>
2024-09-15 17:19:04 +0700

    EXE

---
+++ food/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Sarabun: ["Sarabun", "sans-serif"],
+        Prompt: ["Prompt", "sans-serif"],
+        Mitr: ["Mitr", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
